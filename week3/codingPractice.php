f3ad86885b9a9a11173c2fdf41b3dcfb23b9f1e7
Katie Garcia <katie.garcia.c@gmail.com>
2026-08-24 20:18:52 -0500

    Valid Anagram in php

---
+++ week3/codingPractice.php
@@ -0,0 +1,59 @@
+<?php
+//TO RUN: php codingPractice.php
+// #### Day 1: Valid Anagram
+
+// Given two strings, `s` and `t`, return `true` if `t` is an anagram of `s`.
+// Otherwise, return `false`.
+
+// An anagram is formed by rearranging the letters of another word while using
+// each original letter exactly once.
+
+// **Examples**
+
+// - Input: `s = "anagram"`, `t = "nagaram"`
+// - Output: `true`
+
+// - Input: `s = "rat"`, `t = "car"`
+// - Output: `false`
+
+// **Possible Approaches**
+
+// - **Sorting:** Sort both strings and determine whether the sorted results are
+//   identical. This approach has a time complexity of `O(n log n)`.
+
+// - **Frequency counter:** Count how often each character occurs in `s` and
+//   subtract the corresponding counts while processing `t`. The strings are
+//   anagrams if they have equal lengths and every final count is zero. This
+//   approach has a time complexity of `O(n)`.
+//let counts = {};//reminded myself about global variables
+
+function validAnagram(string $s, string $t): bool
+{
+    if (strlen($s) != strlen($t))
+        return false;
+    $counts = [];
+    for ($i = 0; $i < strlen($s); $i++) {
+        $letter = $s[$i];
+        // PHP way to check if a key does NOT exist
+        if (!isset($counts[$letter])) {
+            $counts[$letter] = 1;
+        } else {
+            $counts[$letter] = $counts[$letter] + 1;
+        }
+    }
+    for ($j = 0; $j < strlen($t); $j++) {
+        $letter = $t[$j];
+        if (!isset($counts[$letter])  || $counts[$letter] === 0) {
+            return false;
+        } else {
+            $counts[$letter] = $counts[$letter] - 1; // Reduce the count by 1
+        }
+    }
+    return true;
+
+}
+
+echo "Day one ";
+echo validAnagram("anagram", "nagaram") ? "TRUE " : "FALSE ";//TRUE
+echo validAnagram("rat", "car") ? "TRUE " : "FALSE "; //false
+?>
